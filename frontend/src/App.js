2648a44ae8a6f1b8b7a87d08953efb5e8cfaa69b
Sowmya Viswanathan <sowmyavi@thoughtworks.com>
2019-07-05 07:45:51 +0530

    Add components in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import StackGrid from "react-stack-grid";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <StackGrid className = "stack-grid" columnWidth={300}>
+            <div>Component 1</div>
+            <div>Component 2</div>
+            <div>Component 3</div>
+          </StackGrid>
+        </div>
+    );
+  }
 }
 
 export default App;
