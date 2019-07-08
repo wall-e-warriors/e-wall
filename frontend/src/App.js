3a44ad0621cd121a8f5578cd589efb87029fee5c
Prasanna <prasanna.venkatesan@kroger.com>
2019-07-08 11:41:07 +0530

    Add prettier to the src code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
-import StackGrid from "react-stack-grid";
-import HealthCheck from "./widgets/healthcheck";
+import React, { Component } from 'react';
+import StackGrid from 'react-stack-grid';
+import HealthCheck from './widgets/healthcheck';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <StackGrid className = "stack-grid" columnWidth={300}>
-            <div>Component 1</div>
-            <div>Component 2</div>
-            <HealthCheck></HealthCheck>
-
-          </StackGrid>
-        </div>
+      <div className="App">
+        <StackGrid className="stack-grid" columnWidth={300}>
+          <div>Component 1</div>
+          <div>Component 2</div>
+          <HealthCheck></HealthCheck>
+        </StackGrid>
+      </div>
     );
   }
 }
--- frontend/src/widgets/healthcheck/index.js
+++ frontend/src/widgets/healthcheck/index.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 function HealthCheck() {
-  const [status, setStatus] = useState("Loading");
+  const [status, setStatus] = useState('Loading');
 
   useEffect(() => {
-    fetch("/healthcheck").then((response) => {
-      response.ok ? setStatus("Success") : setStatus("Error")
-    })
+    fetch('/healthcheck').then(response => {
+      response.ok ? setStatus('Success') : setStatus('Error');
+    });
   });
 
-  return <div >{status}</div >
+  return <div>{status}</div>;
 }
 
-export default HealthCheck
+export default HealthCheck;
