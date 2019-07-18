0701e97084ef03d978b86fc6126c98728a56c7c3
Amit Bikram Dash <ext.amit.bikram@deltatre.it>
2019-07-19 00:39:38 +0530

    Code cleanup - Remove redundant handleChange function

--- frontend/src/widgets/milestone/index.js
+++ frontend/src/widgets/milestone/index.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
-import EditMilestone from './EditMilestone'
-import CreateMilestone from './CreateMilestone'
-import MilestoneView from './MilestoneView'
+import React, { useState } from 'react';
+import EditMilestone from './EditMilestone';
+import CreateMilestone from './CreateMilestone';
+import MilestoneView from './MilestoneView';
 
 function Milestone() {
-  const [mockResponse, setMockResponse] = useState([{
-    "id": 1,
-    "date": "2019-09-12",
-    "description": "Here is the first milestone"
-  }, {
-    "id": 2,
-    "date": "2019-09-25",
-    "description": "Here is the second milestone"
-  }]);
+  const [mockResponse, setMockResponse] = useState([
+    {
+      id: 1,
+      date: '2019-09-12',
+      description: 'Here is the first milestone'
+    },
+    {
+      id: 2,
+      date: '2019-09-25',
+      description: 'Here is the second milestone'
+    }
+  ]);
   const [edit, setEdit] = useState(false);
   const [create, setCreate] = useState(false);
   const [editData, setEditData] = useState(null);
@@ -20,54 +23,78 @@ function Milestone() {
 
   return (
     <div>
-      {!edit && !create && <MilestoneView
-        mockResponse={mockResponse}
-        deleteMilestone={(id) => deleteMilestone(id, mockResponse, setMockResponse)}
-        setCreate={() => setCreate(true)}
-        setEditMode={(response) => setEditMode(setEdit, response, setEditData)}
-      />}
-      {edit && <EditMilestone
-        milestone={editData}
-        onEdit={() => onEdit(setEdit, editData, mockResponse, setMockResponse)}
-        handleChange={(field, value) => handleChange(editData, field, setEditData, value)}
-      />}
-      {create && <CreateMilestone
-        onCreate={() => onCreate(createData, setCreateData, setCreate, mockResponse, setMockResponse)}
-        handleCreate={(field, value) => handleCreate(createData, field, setCreateData, value)}
-      />}
+      {!edit && !create && (
+        <MilestoneView
+          mockResponse={mockResponse}
+          deleteMilestone={id =>
+            deleteMilestone(id, mockResponse, setMockResponse)
+          }
+          setCreate={() => setCreate(true)}
+          setEditMode={response => setEditMode(setEdit, response, setEditData)}
+        />
+      )}
+      {edit && (
+        <EditMilestone
+          milestone={editData}
+          onEdit={() =>
+            onEdit(setEdit, editData, mockResponse, setMockResponse)
+          }
+          handleChange={(field, value) =>
+            setEditData({ ...editData, [field]: value })
+          }
+        />
+      )}
+      {create && (
+        <CreateMilestone
+          onCreate={() =>
+            onCreate(
+              createData,
+              setCreateData,
+              setCreate,
+              mockResponse,
+              setMockResponse
+            )
+          }
+          handleCreate={(field, value) =>
+            handleCreate(createData, field, setCreateData, value)
+          }
+        />
+      )}
     </div>
   );
 }
 
 function handleCreate(createData, field, setCreateData, value) {
-  setCreateData({...createData, [field]: value});
+  setCreateData({ ...createData, [field]: value });
 }
 
 function setEditMode(setEdit, editData, setEditData) {
   setEditData(editData);
-  setEdit(true)
+  setEdit(true);
 }
 
-function onEdit(setEdit, response, mockResponse, setMockResponse) {
-  const index = mockResponse.findIndex(r => r.id === response.id);
-  mockResponse[index] = response;
+function onEdit(setEdit, editData, mockResponse, setMockResponse) {
+  const index = mockResponse.findIndex(r => r.id === editData.id);
+  mockResponse[index] = editData;
   setMockResponse(mockResponse);
-  setEdit(false)
+  setEdit(false);
 }
 
-function handleChange(response, field, setEditData, value) {
-  setEditData({...response, [field]: value});
-}
-
-function onCreate(createData, setCreateData, setCreate, mockResponse, setMockResponse) {
+function onCreate(
+  createData,
+  setCreateData,
+  setCreate,
+  mockResponse,
+  setMockResponse
+) {
   mockResponse.push(createData);
   setMockResponse(mockResponse);
   setCreateData(null);
-  setCreate(false)
+  setCreate(false);
 }
 
 function deleteMilestone(id, mockResponse, setMockResponse) {
-  setMockResponse(mockResponse.filter(r => r.id !== id))
+  setMockResponse(mockResponse.filter(r => r.id !== id));
 }
 
 export default Milestone;
