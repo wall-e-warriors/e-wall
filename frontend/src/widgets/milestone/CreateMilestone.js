820e0c7e7761a4be5ca8c2acaf9d5465d5c1ae1a
Sowmya Viswanathan <sowmyavi@thoughtworks.com>
2019-07-25 07:43:19 +0530

    Add style for edit and create milestone

--- frontend/src/widgets/milestone/CreateMilestone.js
+++ frontend/src/widgets/milestone/CreateMilestone.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CardContent from "@material-ui/core/CardContent";
+import style from "./Milestone.module.css";
 import {createMilestone} from "./MilestoneActions";
 
 function CreateMilestone(props) {
@@ -12,20 +13,31 @@ function CreateMilestone(props) {
       <form noValidate autoComplete="off">
         <div>
           <TextField
+            className={style.textItem}
             id="description"
             required
+            rows="4"
+            variant="outlined"
+            label="Description"
+            margin="dense"
+            multiline
             onChange={(e) => setCreateData({...createData, ['description']: e.target.value})}
           />
         </div>
         <div>
           <TextField
+            className={style.textItem}
             id="date"
             required
+            margin="dense"
+            label="Date"
+            variant="outlined"
             onChange={(e) => setCreateData({...createData, ['date']: e.target.value})}
           />
         </div>
         <div>
           <Button
+            className={style.button}
             id="confirm"
             variant="contained"
             onClick={() => {
--- frontend/src/widgets/milestone/EditMilestone.js
+++ frontend/src/widgets/milestone/EditMilestone.js
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { CardContent } from "@material-ui/core";
 import Slide from "@material-ui/core/Slide";
+import style from "./Milestone.module.css";
 import { updateMilestone } from './MilestoneActions';
 
 function EditMilestone(props) {
@@ -13,20 +14,33 @@ function EditMilestone(props) {
       <CardContent >
         <div >
           <TextField
+            className={style.textItem}
             id="description"
+            required
+            rows="4"
+            variant="outlined"
+            label="Description"
+            margin="dense"
+            multiline
             value={editData.description}
             onChange={(e) => setEditData({...editData, ['description']: e.target.value})}
           />
         </div >
         <div >
           <TextField
+            className={style.textItem}
             id="date"
+            required
+            margin="dense"
+            label="Date"
+            variant="outlined"
             value={editData.date}
             onChange={(e) => setEditData({...editData, ['date']: e.target.value})}
           />
         </div >
         <div >
           <Button
+            className={style.button}
             id="confirm"
             variant="contained"
             onClick={() => {
